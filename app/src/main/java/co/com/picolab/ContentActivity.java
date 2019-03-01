e951da278d766262ef58e62384463e16a4f755f7
Alejandro <edarevalo01@gmail.com>
2019-03-01 01:15:27 -0500

    Movimiento 3D simulado
    -Se corrigen movimientos 
    -Se agrega JavaDoc
    -Se ajusta el PopUp
    -Se agrega nuevos valores para lectura de XML

--- app/src/main/java/co/com/picolab/ContentActivity.java
+++ app/src/main/java/co/com/picolab/ContentActivity.java
@@ -3,9 +3,12 @@ package co.com.picolab;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -31,12 +34,15 @@ public class ContentActivity extends AppCompatActivity {
     private ConstraintLayout layout_popup;
     private VideoView video;
     private TextView textvideo;
+    private TextView texttittle;
 
     private FileInputStream in;
     private ArrayList<Video> videos; //Videos leidos del XML
     private Map<Integer, ImageView> mapImages; //Mapa de miniaturas de videos
     public Map<Integer, Video> mapEntry; //Mapa de descripcion de videos
-    private static final int RED_TAMANO_IMG = 3;
+    private static final int RED_TAMANO_IMG = 2;
+    private static final boolean FRONTIMG = false;
+    private static final int RADIO = 370;
     private int actx = -1, acty = -1; //Variables para mover objetos
     boolean popUpActivated;
 
@@ -49,6 +55,7 @@ public class ContentActivity extends AppCompatActivity {
         layout_popup.setVisibility(View.INVISIBLE);
         video = findViewById(R.id.video);
         textvideo = findViewById(R.id.textvideo);
+        texttittle = findViewById(R.id.texttittle);
 
         mapImages = new TreeMap<>();
         mapEntry = new TreeMap<>();
@@ -61,17 +68,20 @@ public class ContentActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     setVideo(vi);
-                    //CAMBIAR TAMAÑO DE LA PUTA IMAGEN :)
                     layout_popup.setVisibility(View.VISIBLE);
                     layout_popup.setBackground(getResources().getDrawable(R.drawable.fondogif));
                     popUpActivated = true;
-                    //Hacer lo del pop-up
                 }
             });
         }
-
     }
+
+    /**
+     * Metodo para establecer videos en el PopUp
+     * @param id id del video respecto al objeto
+     */
     private void setVideo(int id){
+        texttittle.setText(mapEntry.get(id).getTitulo());
         textvideo.setText(mapEntry.get(id).getDescripcion());
         File videoFile;
         String videoName = mapEntry.get(id).getNombre();
@@ -80,6 +90,13 @@ public class ContentActivity extends AppCompatActivity {
         video.start();
     }
 
+    /**
+     * Metodo para ubicar imagenes en el Layout
+     * @param size Tamano de la imagen
+     * @param left ubicacion X
+     * @param top ubicacion Y
+     * @param img Imagen a colocar
+     */
     private void setImages(int size, int left, int top, ImageView img){
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(size, size);
         params.leftMargin = left;
@@ -87,6 +104,10 @@ public class ContentActivity extends AppCompatActivity {
         layout_principal.addView(img, params);
     }
 
+    /**
+     * Metodo para ubicar miniaturas de videos en pantalla de contenido
+     * @param videos
+     */
     private void printInfoVideos(ArrayList<Video> videos){
         for(Video video: videos){
             ImageView img = new ImageView(this);
@@ -125,58 +146,152 @@ public class ContentActivity extends AppCompatActivity {
         return false;
     }
 
-    private void moveImg(int x0, int x1, int y0, int y1){
+    /**
+     * Metodo para calcular posiciones al mover las imagenes
+     * @param x0
+     * @param x1
+     * @param y0
+     * @param y1
+     */
+    private void moveImg(int x0, int x1, int y0, int y1) {
         for(final Integer i : mapImages.keySet()){
             ImageView im = mapImages.get(i);
-            int[] pos = posImg(x0, x1, y0, y1);
-
-            if(pos[2] < 0){
-                for(int j = pos[0]; j < 0; j++){
-                    if(im.getX() <= 0){
-                        im.setX(1279);
-                        mapEntry.get(i).setPosx(1279);
+            if(mapEntry.get(i).getFrente()) {
+                int[] pos = posImg(x0, x1, y0, y1);
+                if (pos[2] < 0) {
+                    for (int j = pos[0]; j < 0; j++) {
+                        if (distance(640, mapEntry.get(i).getPosx(), 400, mapEntry.get(i).getPosy()) >= RADIO) {
+                            im.setX(mapEntry.get(i).getPosx() + 3);
+                            mapEntry.get(i).setPosx((int)im.getX());
+                            mapEntry.get(i).setFrente(FRONTIMG);
+                        }
+                        else {
+                            im.setX(mapEntry.get(i).getPosx() - 1);
+                            mapEntry.get(i).setPosx(mapEntry.get(i).getPosx() - 1);
+                        }
                     }
-                    im.setX(mapEntry.get(i).getPosx() - 1);
-                    mapEntry.get(i).setPosx(mapEntry.get(i).getPosx() - 1);
+                } else {
+                    for (int j = 0; j < pos[0]; j++) {
+                        if (distance(640, mapEntry.get(i).getPosx(), 400, mapEntry.get(i).getPosy()) >= RADIO) {
+                            //1020
+                            im.setX(mapEntry.get(i).getPosx() - 3);
+                            mapEntry.get(i).setPosx((int)im.getX());
+                            mapEntry.get(i).setFrente(FRONTIMG);
+                        }
+                        else {
+                            im.setX(mapEntry.get(i).getPosx() + 1);
+                            mapEntry.get(i).setPosx(mapEntry.get(i).getPosx() + 1);
+                        }
+                    }
                 }
+                if (pos[3] < 0) {
+                    for (int j = pos[1]; j < 0; j++) {
+                        if (distance(640, mapEntry.get(i).getPosx(), 400, mapEntry.get(i).getPosy()) >= RADIO) {
+                            //20
+                            im.setY(mapEntry.get(i).getPosy() + 3);
+                            mapEntry.get(i).setPosy((int)im.getY());
+                            mapEntry.get(i).setFrente(FRONTIMG);
+                        }
+                        else {
+                            im.setY(mapEntry.get(i).getPosy() - 1);
+                            mapEntry.get(i).setPosy(mapEntry.get(i).getPosy() - 1);
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < pos[1]; j++) {
+                        if (distance(640, mapEntry.get(i).getPosx(), 400, mapEntry.get(i).getPosy()) >= RADIO) {
+                            //780
+                            im.setY(mapEntry.get(i).getPosy() - 3);
+                            mapEntry.get(i).setPosy((int)im.getY());
+                            mapEntry.get(i).setFrente(FRONTIMG);
+                        }
+                        else {
+                            im.setY(mapEntry.get(i).getPosy() + 1);
+                            mapEntry.get(i).setPosy(mapEntry.get(i).getPosy() + 1);
+                        }
+                    }
+                }
+
+                int tamano = tamanoImg(mapEntry.get(i).getPosx(), mapEntry.get(i).getPosy())/ RED_TAMANO_IMG;
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(tamano, tamano);
+                im.setLayoutParams(params);
             }
             else{
-                for (int j = 0; j < pos[0]; j++){
-                    if(im.getX() + im.getWidth() > 1279){
-                        im.setX(0);
-                        mapEntry.get(i).setPosx(0);
+                int[] pos = posImg(x0, x1, y0, y1);
+                if (-pos[2] < 0) {
+                    for (int j = 0; j < pos[0]; j++) {
+                        //derecha
+                        if (distance(640, mapEntry.get(i).getPosx(), 400, mapEntry.get(i).getPosy()) >= RADIO) {
+                            //260
+                            im.setX(mapEntry.get(i).getPosx() + 3);
+                            mapEntry.get(i).setPosx((int)im.getX());
+                            mapEntry.get(i).setFrente(!FRONTIMG);
+                        }
+                        else {
+                            im.setX(mapEntry.get(i).getPosx() - 1);
+                            mapEntry.get(i).setPosx(mapEntry.get(i).getPosx() - 1);
+                        }
                     }
-                    im.setX(mapEntry.get(i).getPosx() + 1);
-                    mapEntry.get(i).setPosx(mapEntry.get(i).getPosx() + 1);
-                }
-            }
-            if(pos[3] < 0){
-                for(int j = pos[1]; j < 0; j++){
-                    if(im.getY() <= 0){
-                        im.setY(799);
-                        mapEntry.get(i).setPosy(799);
+                } else {
+                    for (int j = pos[0]; j < 0; j++) {
+                        //izquierda
+                        if (distance(640, mapEntry.get(i).getPosx(), 400, mapEntry.get(i).getPosy()) >= RADIO) {
+                            //1020
+                            im.setX(mapEntry.get(i).getPosx() - 3);
+                            mapEntry.get(i).setPosx((int)im.getX());
+                            mapEntry.get(i).setFrente(!FRONTIMG);
+                        }
+                        else {
+                            im.setX(mapEntry.get(i).getPosx() + 1);
+                            mapEntry.get(i).setPosx(mapEntry.get(i).getPosx() + 1);
+                        }
                     }
-                    im.setY(mapEntry.get(i).getPosy() - 1);
-                    mapEntry.get(i).setPosy(mapEntry.get(i).getPosy() - 1);
                 }
-            }
-            else {
-                for(int j = 0; j < pos[1]; j++){
-                    if(im.getY() + im.getHeight() > 799){
-                        im.setY(0);
-                        mapEntry.get(i).setPosy(0);
+                if (-pos[3] < 0) {
+                    for (int j = 0; j < pos[1]; j++) {
+                        //abajo
+                        if (distance(640, mapEntry.get(i).getPosx(), 400, mapEntry.get(i).getPosy()) >= RADIO) {
+                            //20
+                            im.setY(mapEntry.get(i).getPosy() + 3);
+                            mapEntry.get(i).setPosy((int)im.getY());
+                            mapEntry.get(i).setFrente(!FRONTIMG);
+                        }
+                        else {
+                            im.setY(mapEntry.get(i).getPosy() - 1);
+                            mapEntry.get(i).setPosy(mapEntry.get(i).getPosy() - 1);
+                        }
                     }
-                    im.setY(mapEntry.get(i).getPosy() + 1);
-                    mapEntry.get(i).setPosy(mapEntry.get(i).getPosy() + 1);
+                } else {
+                    for (int j = pos[1]; j < 0; j++) {
+                        //arriba
+                        if (distance(640, mapEntry.get(i).getPosx(), 400, mapEntry.get(i).getPosy()) >= RADIO) {
+                            //780
+                            im.setY(mapEntry.get(i).getPosy() - 3);
+                            mapEntry.get(i).setPosy((int)im.getY());
+                            mapEntry.get(i).setFrente(!FRONTIMG);
+                        }
+                        else {
+                            im.setY(mapEntry.get(i).getPosy() + 1);
+                            mapEntry.get(i).setPosy(mapEntry.get(i).getPosy() + 1);
+                        }
+                    }
                 }
-            }
 
-            int tamano = tamanoImg(mapEntry.get(i).getPosx(), mapEntry.get(i).getPosy()) / RED_TAMANO_IMG;
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(tamano, tamano);
-            im.setLayoutParams(params);
+            int tamano = tamanoImg(mapEntry.get(i).getPosx(), mapEntry.get(i).getPosy()) / (RED_TAMANO_IMG*2);
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(tamano, tamano);
+                im.setLayoutParams(params);
+            }
         }
     }
 
+    /**
+     * Metodo para calcular la dirección en la que se moverán los objetos
+     * @param x0 XAnterior
+     * @param x1 XActual
+     * @param y0 YAnterior
+     * @param y1 YActual
+     * @return Dirección hacia donde se mueven los objetos
+     */
     private int[] posImg(int x0, int x1, int y0, int y1) {
         int difx = x1 - x0;
         int dify = y1 - y0;
@@ -191,38 +306,51 @@ public class ContentActivity extends AppCompatActivity {
         return new int[]{difx, dify, sigx, sigy};
     }
 
+    /**
+     * Metodo para establecer tamaño respecto a distancia con bordes
+     * @param x Posicion X
+     * @param y Posicion Y
+     * @return Tamano de la imagen
+     */
     private int tamanoImg(int x, int y){
         int n = 0;
         if(x < 640 && y < 400){
-            n = distance(x, 0, y, 0);
+            n = distance(x, 260, y, 20);
         }
         if(x > 640 && y < 400){
-            n = distance(x, 1280, y, 0);
+            n = distance(x, 1020, y, 20);
         }
         if(x < 640 && y > 400){
-            n = distance(x, 0, y, 800);
+            n = distance(x, 260, y, 780);
         }
         if(x > 640 && y > 400){
-            n = distance(x, 1280, y, 800);
+            n = distance(x, 1020, y, 780);
         }
         return n;
     }
 
+    /**
+     * Metodo para calcular distancia ecuclidiana
+     * @param x X actual
+     * @param x0 X inicial
+     * @param y Y actual
+     * @param y0 Y inicial
+     * @return Distancia euclidiana entre dos puntos
+     */
     private int distance(int x, int x0, int y, int y0){
         return (int)Math.sqrt((Math.pow(x-x0,2)) + (Math.pow(y-y0,2)));
     }
 
+    /** Metodo para leer XML */
     private void parseXML(){
         XmlPullParserFactory parserFactory;
         try {
             parserFactory = XmlPullParserFactory.newInstance();
             XmlPullParser parser = parserFactory.newPullParser();
-            //in = getAssets().open("videos.xml");
-            //Nuevo de aca
-            File storageDir = new File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),"videos.xml");
+            //in = getAssets().open("videos.xml"); //Lectura de XML desde proyecto
+            File storageDir = new File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),"videos.xml"); //Leer XML desde Dispositivo
             String s = storageDir.getAbsolutePath();
             in = new FileInputStream(s);
-            //A aca
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
             parser.setInput(in, null);
             processParsing(parser);
@@ -233,6 +361,12 @@ public class ContentActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Metodo para parsear videos desde el xml y convertirlo en objetos
+     * @param parser Archivo leido desde videos.xml
+     * @throws IOException
+     * @throws XmlPullParserException
+     */
     private void processParsing(XmlPullParser parser) throws IOException, XmlPullParserException {
         videos = new ArrayList<>();
         int eventType = parser.getEventType();
@@ -259,6 +393,10 @@ public class ContentActivity extends AppCompatActivity {
                             currentVideo.setPosx(Integer.parseInt(parser.nextText()));
                         } else if("posy".equals(eltName)){
                             currentVideo.setPosy(Integer.parseInt(parser.nextText()));
+                        } else if("frente".equals(eltName)){
+                            currentVideo.setFrente(Boolean.parseBoolean(parser.nextText().toLowerCase()));
+                        }else{
+                            Log.e("Error en elemento", eltName + " este elemento no existe");
                         }
                     }
                     break;
@@ -270,7 +408,7 @@ public class ContentActivity extends AppCompatActivity {
         layout_principal.addView(layout_popup);
     }
 
-    //Metodo para tener pantalla completa
+    /** Metodo para Pantalla completa */
     @Override
     protected void onStart() {
         super.onStart();
